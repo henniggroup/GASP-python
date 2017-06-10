b144ef4fa6eee7c5702b1f4c2c89e69be4b68ce3
brevard <bcr48@cornell.edu>
2017-06-10 18:38:42 -0400

    Created new module to hold surrogate models.

---
+++ gasp/surrogate_models.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Copyright (c) Henniggroup.
+# Distributed under the terms of the MIT License.
+
+from __future__ import division, unicode_literals, print_function
+
+
+"""
+Surrogate Models module:
+
+TODO: description of interface and classes
+"""
